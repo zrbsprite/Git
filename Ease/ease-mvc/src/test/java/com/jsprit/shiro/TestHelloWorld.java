09ca2ab1b50f98d5704942f258f04fbe8e14112c
zrbsprite <zhangribo@sina.cn>
2015-04-27 18:18:56 +0800

    添加ztree支持
    添加ztree支持

---
+++ Ease/ease-mvc/src/test/java/com/jsprit/shiro/TestHelloWorld.java
@@ -0,0 +1,52 @@
+/**
+ * File Name:TestHelloWorld.java
+ * @Description: 
+ * Copyright 2012 China Life Insurance Company Ltd.
+ * @author:ZRB
+ * @version:v1.0
+ * Createdate:2015年4月27日上午9:18:57
+ */
+package com.jsprit.shiro;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**描述：<br>
+ * 作者：ZRB <br>
+ * 修改日期：2015年4月27日上午9:18:57 <br>
+ * E-mail:  <br> 
+ */
+public class TestHelloWorld {
+
+	@Test
+	public void test() {
+		//1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager  
+	    Factory<org.apache.shiro.mgt.SecurityManager> factory =  
+	            new IniSecurityManagerFactory("classpath:shiro.ini");  
+	    //2、得到SecurityManager实例 并绑定给SecurityUtils  
+	    org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();  
+	    SecurityUtils.setSecurityManager(securityManager);  
+	    //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）  
+	    Subject subject = SecurityUtils.getSubject();  
+	    UsernamePasswordToken token = new UsernamePasswordToken("zhangsan", "123");  
+	  
+	    try {  
+	        //4、登录，即身份验证  
+	        subject.login(token);  
+	    } catch (AuthenticationException e) {  
+	        //5、身份验证失败  
+	    }  
+	  
+	    Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录  
+	  
+	    //6、退出  
+	    subject.logout();  
+	}
+
+}
