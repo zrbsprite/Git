06bec47b721a3a7dfd769083d486c89b3f4aabb7
zrbsprite <zhangribo@sina.cn>
2015-06-05 16:53:39 +0800

    Menu
    Menu

---
+++ Ease/ease-mvc/src/main/java/com/jsprite/web/controller/MenuController.java
@@ -0,0 +1,18 @@
+/**
+ * File Name:MenuController.java
+ * @Description: 
+ * Copyright 2015 Ease Company Ltd.
+ * @author:ZRB
+ * @version:v1.0
+ * Createdate:2015年6月5日下午4:16:36
+ */
+package com.jsprite.web.controller;
+
+/**描述：<br>
+ * 作者：ZRB <br>
+ * 修改日期：2015年6月5日下午4:16:36 <br>
+ * E-mail:  <br> 
+ */
+public class MenuController {
+
+}
---
+++ Ease/ease-mvc/src/main/java/com/jsprite/web/service/MenuService.java
@@ -0,0 +1,21 @@
+/**
+ * File Name:MenuService.java
+ * @Description: 
+ * Copyright 2015 Ease Company Ltd.
+ * @author:ZRB
+ * @version:v1.0
+ * Createdate:2015年6月5日下午4:17:11
+ */
+package com.jsprite.web.service;
+
+import com.jsprite.web.commons.BaseService;
+
+/**描述：<br>
+ * 作者：ZRB <br>
+ * 修改日期：2015年6月5日下午4:17:11 <br>
+ * E-mail:  <br> 
+ * @param <T>
+ */
+public interface MenuService<T> extends BaseService<T>{
+
+}
