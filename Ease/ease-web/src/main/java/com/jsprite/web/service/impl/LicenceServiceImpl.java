4d9bb18d6a1172b2ebb127926362969237a49640
zrbsprite <zhangribo@sina.cn>
2016-01-20 11:33:07 +0800

    lombok

--- Ease/ease-web/src/main/java/com/jsprite/web/service/impl/LicenceServiceImpl.java
+++ Ease/ease-web/src/main/java/com/jsprite/web/service/impl/LicenceServiceImpl.java
@@ -2,11 +2,15 @@ package com.jsprite.web.service.impl;
 
 import javax.annotation.Resource;
 
+import org.springframework.stereotype.Service;
+
 import com.jsprite.core.BaseService;
+import com.jsprite.core.IDao;
 import com.jsprite.web.dao.LicenceDao;
 import com.jsprite.web.model.LicenceModel;
 import com.jsprite.web.service.LicenceService;
 
+@Service("licenceService")
 public class LicenceServiceImpl extends BaseService<LicenceModel> implements LicenceService {
 
 	private LicenceDao licenceDao;
@@ -18,5 +22,17 @@ public class LicenceServiceImpl extends BaseService<LicenceModel> implements Lic
 	@Resource
 	public void setLicenceDao(LicenceDao licenceDao) {
 		this.licenceDao = licenceDao;
+	}
+
+	/**方法名称：getBaseDao <br>
+	 * 描述： <br>
+	 * 作者：ZRB <br>
+	 * 修改日期：2016年1月20日上午11:27:17 
+	 * @see com.jsprite.core.BaseService#getBaseDao() 
+	 * @return
+	 */
+	@Override
+	public IDao<LicenceModel> getBaseDao() {
+		return licenceDao;
 	}
 }
