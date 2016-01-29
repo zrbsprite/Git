52f6589b1e0dff616a804071f5a73b70188fbf5a
zrbsprite <zhangribo@sina.cn>
2016-01-29 17:49:38 +0800

    redis
    redis

--- mangix/mangix-redis/src/test/java/com/penzias/mangix/redis/HelloRedisTest.java
+++ mangix/mangix-redis/src/test/java/com/penzias/mangix/redis/HelloRedisTest.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -51,28 +52,24 @@ public class HelloRedisTest {
 	public void testBasicString() {
 		// -----添加数据----------
 		jedis.set("name", "minxr");// 向key-->name中放入了value-->minxr
-		System.out.println(jedis.get("name"));// 执行结果：minxr
+		Assert.assertEquals("minxr", jedis.get("name"));
 
 		// -----修改数据-----------
 		// 1、在原来基础上修改
 		jedis.append("name", "jarorwar"); // 很直观，类似map 将jarorwar
-											// append到已经有的value之后
-		System.out.println(jedis.get("name"));// 执行结果:minxrjarorwar
+		Assert.assertEquals("minxrjarorwar", jedis.get("name"));
 
 		// 2、直接覆盖原来的数据
-		jedis.set("name", "闵晓荣");
-		System.out.println(jedis.get("name"));// 执行结果：闵晓荣
+		jedis.set("name", "zhangribo");
+		Assert.assertEquals("zhangribo", jedis.get("name"));
 
 		// 删除key对应的记录
 		jedis.del("name");
-		System.out.println(jedis.get("name"));// 执行结果：null
+		Assert.assertNull(jedis.get("name"));
 
-		/**
-		 * mset相当于 jedis.set("name","minxr"); jedis.set("jarorwar","闵晓荣");
-		 */
-		jedis.mset("name", "minxr", "jarorwar", "闵晓荣");
-		System.out.println(jedis.mget("name", "jarorwar"));
-
+		jedis.mset("name", "minxr", "age", "28");
+		Assert.assertEquals("minxr", jedis.mget("name", "age").get(0));
+		Assert.assertEquals("28", jedis.mget("name", "age").get(1));
 	}
 
 	/**
@@ -81,24 +78,28 @@ public class HelloRedisTest {
 	@Test
 	public void testMap() {
 		Map<String, String> user = new HashMap<String, String>();
-		user.put("name", "minxr");
-		user.put("pwd", "password");
+		user.put("name", "zhangwenluo");
+		user.put("pwd", "132456");
 		jedis.hmset("user", user);
 		// 取出user中的name，执行结果:[minxr]-->注意结果是一个泛型的List
 		// 第一个参数是存入redis中map对象的key，后面跟的是放入map中的对象的key，后面的key可以跟多个，是可变参数
 		List<String> rsmap = jedis.hmget("user", "name");
-		System.out.println(rsmap);
+		Assert.assertEquals("zhangwenluo", rsmap.get(0));
 
 		// 删除map中的某个键值
-		// jedis.hdel("user","pwd");
-		System.out.println(jedis.hmget("user", "pwd")); // 因为删除了，所以返回的是null
-		System.out.println(jedis.hlen("user")); // 返回key为user的键中存放的值的个数1
-		System.out.println(jedis.exists("user"));// 是否存在key为user的记录 返回true
-		System.out.println(jedis.hkeys("user"));// 返回map对象中的所有key [pwd, name]
-		System.out.println(jedis.hvals("user"));// 返回map对象中的所有value [minxr,
-												// password]
+		jedis.hdel("user","pwd");
+		Assert.assertNull(jedis.hmget("user", "pwd"));
+		// 返回key为user的键中存放的值的个数1
+		Assert.assertEquals(1l, jedis.hlen("user").longValue());
+		// 是否存在key为user的记录 返回true
+		Assert.assertTrue(jedis.exists("user"));
+		// 返回map对象中的所有key [pwd, name]
+		Set<String> set = jedis.hkeys("user");
+		System.out.println(set);
+		// 返回map对象中的所有value [minxr,password]
+		System.out.println(jedis.hvals("user"));
 
-		Iterator<String> iter = jedis.hkeys("user").iterator();
+		Iterator<String> iter = set.iterator();
 		while (iter.hasNext()) {
 			String key = iter.next();
 			System.out.println(key + ":" + jedis.hmget("user", key));
@@ -121,6 +122,7 @@ public class HelloRedisTest {
 		// 再取出所有数据jedis.lrange是按范围取出，
 		// 第一个是key，第二个是起始位置，第三个是结束位置，jedis.llen获取长度 -1表示取得所有
 		System.out.println(jedis.lrange("java framework", 0, -1));
+		Assert.assertEquals(3l,  jedis.llen("java framework").longValue());
 	}
 
 	/**
