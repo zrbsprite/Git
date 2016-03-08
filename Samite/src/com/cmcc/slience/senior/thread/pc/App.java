07d24dd745be60c87d63a970f4bdb9157a33a07e
zrbsprite <zhangribo@sina.cn>
2016-03-08 17:54:25 +0800

    生产者和消费者

--- Samite/src/com/cmcc/slience/senior/thread/pc/App.java
+++ Samite/src/com/cmcc/slience/senior/thread/pc/App.java
@@ -5,8 +5,8 @@ public class App {
 	public static void main(String[] args) {
 		Factory factory = new Factory();
 		for(int i=0; i<1; i++){
-			new Thread(new Productor(factory), "product-"+i).start();;
-			new Thread(new Consume(factory), "consume-"+i).start();;
+			new Thread(new Productor(factory), "product-"+i).start();
+			new Thread(new Consume(factory), "consume-"+i).start();
 		}
 	}
 }
