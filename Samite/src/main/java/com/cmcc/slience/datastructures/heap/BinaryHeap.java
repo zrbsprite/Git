8bd83a8ed0a6378109449afca6e3cc44b8d82aa5
zhangribo@sina.cn <zhangribo@sina.cn>
2016-11-16 10:57:55 +0800

    二叉堆，优先队列

--- Samite/src/main/java/com/cmcc/slience/datastructures/heap/BinaryHeap.java
+++ Samite/src/main/java/com/cmcc/slience/datastructures/heap/BinaryHeap.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
  */
 public class BinaryHeap<T extends Comparable<T>> {
 
-	private final int defaultSize = 10;
+	private final int defaultSize = 11;
 
 	private int realSize = 0;
 	
@@ -23,6 +23,14 @@ public class BinaryHeap<T extends Comparable<T>> {
 
 	public BinaryHeap(int size) {
 		heap = new Object[size];
+	}
+	
+	public BinaryHeap(T[] items) {
+		heap = new Object[items.length];
+		for(T t : items){
+			add(t);
+			realSize++;
+		}
 	}
 
 	/**
@@ -50,16 +58,77 @@ public class BinaryHeap<T extends Comparable<T>> {
 	 * @return 最小元素
 	 */
 	public T deleteMin() {
-		int rootIndex = realSize/2;
-		T element = (T) heap[rootIndex];
-		heap[rootIndex] = null;
-		return null;
+		T root = findMin();
+		//首先被挖空的是根节点
+		int hole = 1;
+		//将根节点删除，并将最后一个叶子节点放于根处，同时堆的大小减一
+		heap[hole] = heap[realSize--];
+		percolateDown(hole);
+		return root;
 	}
 
-	public void delete(T element){
-		
+	/**
+	 * 方法名称: findMin<br>
+	 * 描述：最小堆的最小元素应该在根部，根据堆序性可知就是数组的第一个元素index:1<br/>
+	 * 作者: ZRB<br/>
+	 * 修改日期：2016年11月16日上午8:57:31<br/>
+	 * @return
+	 */
+	public T findMin(){
+		return (T)heap[1];
 	}
 	
+	public void buildHeap(T[] items){
+		realSize = items.length;
+		heap = new Object[realSize];
+		int index=1;
+		for(T t : items){
+			heap[index++]=t;
+		}
+		buildHeap();
+	}
+	
+	
+	/**
+	 * 方法名称: buildHeap<br>
+	 * 描述：		<br/>
+	 * 作者: ZRB<br/>
+	 * 修改日期：2016年11月16日上午10:46:11<br/>
+	 */
+	private void buildHeap() {
+		for(int i=realSize/2;i>0;i--){
+			percolateDown(i);
+		}
+	}
+
+	/**
+	 * 方法名称: percolateDown<br>
+	 * 描述：	下滤<br/>
+	 * 作者: ZRB<br/>
+	 * 修改日期：2016年11月16日上午10:52:18<br/>
+	 * @param i
+	 */
+	private void percolateDown(int hole) {
+		T element = (T) heap[hole];
+		//循环找到新的合适的根节点
+		int child;
+		//hole*2<=realSize -> 循环的条件是左子节点存在
+		//hole代表当前的空是哪个
+		for(;hole*2<=realSize;hole=child){
+			//找到左子节点
+			child = hole*2;
+			//左子节点不是最后一个节点,并且左子节点小于右子节点
+			if(child!=realSize && ((T)heap[child+1]).compareTo((T)heap[child])<0)
+				child++;
+			if(((T)heap[child]).compareTo(element)<0){
+				heap[hole] = heap[child]; 
+			}else{
+				break;
+			}
+		}
+		heap[hole] = element;
+	}
+
 	// 扩容
 	private void ensureCapacity(int minCapacity) {
 		if (minCapacity - heap.length > 0)
